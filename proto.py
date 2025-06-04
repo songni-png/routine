00e97943e6d22ad1ebfbf2f74d7435ae294fac77
songni-png <soyoenjune0@gmail.com>
2025-06-04 23:53:12 +0900

    Update proto.py

--- proto.py
+++ proto.py
@@ -170,8 +170,9 @@ if sampled_df is not None:
                     st.info("📭 관련 장소가 없습니다.")
                 else:
                     st.markdown(f"#### 🏷️ '{row['CATEGORY']}' 및 유사 카테고리 관련 추천 장소")
-                    for _, mp in more_places.iterrows():
-                        with st.container():
+                    cols = st.columns(3)
+                    for index, mp in enumerate(more_places.iterrows()):
+                        with cols[index]:
                             st.markdown(f"##### 🏷️ {mp['NAME']}")
                             st.markdown(f"📍 **위치:** {mp['LOCATION']}")
                             st.markdown(f"🏷️ **태그:** {mp.get('TAG', '없음')}")
